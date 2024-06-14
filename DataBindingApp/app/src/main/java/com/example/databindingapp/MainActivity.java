5258e7db954ba6572a4ded060d4c83c2569f141a
anjali29singh <anjalisingh930260@gmail.com>
2024-06-14 11:31:09 +0530

    data binding

---
+++ DataBindingApp/app/src/main/java/com/example/databindingapp/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.databindingapp;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+
+import com.example.databindingapp.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding activityMainBinding;
+
+    private MyClickHandler myClickHandler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        Person p1 = new Person("Anjali","anjalisingh.com");
+        activityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        activityMainBinding.setPerson(p1);
+
+        myClickHandler = new MyClickHandler(this);
+        activityMainBinding.setClickHandler(myClickHandler);
+    }
+}
---
+++ DataBindingApp/app/src/main/java/com/example/databindingapp/MyClickHandler.java
@@ -0,0 +1,22 @@
+package com.example.databindingapp;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.Toast;
+
+public class MyClickHandler {
+
+    Context context;
+
+    public MyClickHandler(Context context){
+
+        this.context = context;
+    }
+
+    public  void onButtonClick(View view){
+
+        Toast.makeText(context, "Button is clicked", Toast.LENGTH_SHORT).show();
+
+
+    }
+}
